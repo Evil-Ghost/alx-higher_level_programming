c577598032e7b43c2673001621e1e7211747bba8
evil-ghost <danielaisosajoseph@outlook.com>
2023-12-28 08:33:42 -0800

    Task 2: make correction

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,6 +8,10 @@ if len(sys.argv) <= 1:
     print("{} arguments.".format(len(sys.argv) - 1))
     sys.exit(0)
 
-print("{} arguments:".format(len(sys.argv) - 1))
+if (len(sys.argv) == 2):
+    print("{} argument:".format(len(sys.argv) - 1))
+else:
+    print("{} arguments:".format(len(sys.argv) - 1))
+
 for index, argv in enumerate(sys.argv[1:]):
     print("{}: {}".format(index + 1, argv))

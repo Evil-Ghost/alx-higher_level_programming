7b710214800e5cc2d5899fc1e462193cc6b6abb9
evil-ghost <danielaisosajoseph@outlook.com>
2023-12-30 11:53:38 -0800

    Task 3: fix pycodestyle error

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    if my_list == None:
+    if my_list is None:
         return
-    my_list.reverse()
 
+    my_list.reverse()
     for x in my_list:
         print("{:d}".format(x))

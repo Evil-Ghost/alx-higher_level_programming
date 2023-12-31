fbe8ed8da865f0ec7ab1b48f140b0b7bb88bf363
evil-ghost <danielaisosajoseph@outlook.com>
2023-12-31 11:00:40 -0800

    Task 10: complete task

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if my_list == []:
+        return None
+    true_false = []
+    for n in my_list:
+        if n % 2 == 0:
+            true_false.append(True)
+        else:
+            true_false.append(False)
+    return true_false

671c6dd05a366fc7a40f9ccdbf466340803e94c5
evil-ghost <danielaisosajoseph@outlook.com>
2024-01-05 17:24:25 -0800

    Task 12: change conditon to 'or'

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@
 def roman_to_int(roman_string):
     rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
-    if roman_string is None and type(roman_string) is not str:
+    if roman_string is None or type(roman_string) is not str:
         return num
 
     prev = 0

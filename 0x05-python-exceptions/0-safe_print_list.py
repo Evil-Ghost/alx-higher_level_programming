ee9a4a568bc84327d59069652c777dfc7b8d9eb9
evil-ghost <danielaisosajoseph@outlook.com>
2024-01-09 17:28:09 -0800

    Task 0: make correction

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if my_list == [] or my_list is None:
-        return None
+    if my_list == [] or my_list is None or x == 0:
+        print()
+        return 0
 
     i = 0
     while i < x:
